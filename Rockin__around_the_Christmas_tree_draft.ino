8018c8725c159a6616108dbae59873e27ee23465
Henry <henry.bryant70@gmail.com>
2017-12-21 12:50:30 -0800

    Changed things

--- Rockin__around_the_Christmas_tree_draft.ino
+++ Rockin__around_the_Christmas_tree_draft.ino
@@ -12,85 +12,62 @@ const int Bb4 = 466;
 const int R = 0;
 
 int buzzerPin = 14;
-int numNotes = 126;
-int numNotes2 = 63;
+int numNotes = 190;
 // notes in the melody:
 int melody[] = {
   Eb4, R, Eb4, R, Eb4, R, F4, R, F4, R,
   Eb4, R, Eb4, R, F4, F4, R, Eb4, R, Eb4, R,
-  G4, R, F4, R, Db4, R, Bb3, R, G3, G3, R,
+  G4, R, F4, R, Db4, R, Bb3, R, G3, G3, R, R,
 
   F4, R, F4, R, F4, R, G4, R, G4, R,
   F4, R, F4, R, G4, G4, R, F4, R, F4, R,
-  Eb4, R, E4, R, F4, R, Db4, R, C4, R,
+  Eb4, R, E4, R, F4, R, Db4, R, C4, R, R,
 
   Eb4, R, Eb4, R, Eb4, R, F4, R, F4, R,
   Eb4, R, Eb4, R, F4, F4, R, Eb4, R, Eb4, R,
-  G4, R, F4, R, Db4, R, Bb3, R, G3, G3, R,
+  G4, R, F4, R, Db4, R, Bb3, R, G3, G3, R, R,
 
   F4, R, F4, R, F4, R, G4, R, G4, R,
   F4, R, F4, R, G4, G4, R, F4, R, F4, R,
-  Eb4, R, E4, R, F4, R, G3, R, Ab3, R,
+  Eb4, R, E4, R, F4, R, G3, R, Ab3, R, R,
+
+  F4, R, E4, R, F4, R, E4, R,
+  F4, R, Ab3, R, Db4, R, F4, R,
+  G4, R, Eb4, R, C4, R, Eb4, R, G4, R, R,
+
+  Ab4, R, G4, R, Ab4, R, G4, R,
+  Ab4, R, F4, R, G4, R, Ab4, R,
+  Bb4, R, R, Ab4, R, G4, R, F4, R,
+  Eb4, R, F4, R, G4, R, Eb4, R,
 };
-/*
-int melody2[] = {
-  Eb4, R, Eb4, R, Eb4, R, F4, R, F4, R,
-  Eb4, R, Eb4, R, F4, F4, R, Eb4, R, Eb4, R,
-  G4, R, F4, R, Db4, R, Bb3, R, G3, G3, R,
-
-  F4, R, F4, R, F4, R, G4, R, G4, R,
-  F4, R, F4, R, G4, G4, R, F4, R, F4, R,
-  Eb4, R, E4, R, F4, R, G3, R, Ab3, R,
-};
-
-  F4, E4, F4, E4,
-  F4, Ab3, Db4, F4,
-  G4, Eb4, C4, Eb4, G4,
-
-  Ab4, G4, Ab4, G4,
-  Ab4, F4, G4, Ab4,
-  Bb4, R, Ab4, G4, F4,
-  Eb4, F4, G4, Eb4
-  */
 
 // note durations: 4 = quarter note, 16 = eighth note, etc.:
 int noteDurations[] = {
   6, 16, 12, 16, 4, 16, 4, 16, 4, 16,
   4, 16, 6, 16, 12, 4, 16, 6, 16, 12, 16,
-  4, 16, 4, 16, 4, 16, 6, 16, 12, 2, 2,
+  4, 16, 4, 16, 4, 16, 6, 16, 12, 2, 2, 16,
   
   6, 16, 12, 16, 4, 16, 4, 16, 4, 16,
   4, 16, 6, 16, 12, 4, 16, 6, 16, 12, 16,
-  4, 16, 4, 16, 4, 16, 4, 16, 2, 2,
+  4, 16, 4, 16, 4, 16, 4, 16, 2, 2, 16,
 
   6, 16, 12, 16, 4, 16, 4, 16, 4, 16,
   4, 16, 6, 16, 12, 4, 16, 6, 16, 12, 16,
-  4, 16, 4, 16, 4, 16, 6, 16, 12, 2, 2,
+  4, 16, 4, 16, 4, 16, 6, 16, 12, 2, 2, 16,
   
   6, 16, 12, 16, 4, 16, 4, 16, 4, 16,
   4, 16, 6, 16, 12, 4, 16, 6, 16, 12, 16,
-  4, 16, 4, 16, 4, 16, 4, 16, 2, 2,
-};
-/*
-int noteDurations2[] = {
-  6, 16, 12, 16, 4, 16, 4, 16, 4, 16,
-  4, 16, 6, 16, 12, 4, 16, 6, 16, 12, 16,
-  4, 16, 4, 16, 4, 16, 6, 16, 12, 2, 2,
-  
-  6, 16, 12, 16, 4, 16, 4, 16, 4, 16,
-  4, 16, 6, 16, 12, 4, 16, 6, 16, 12, 16,
-  4, 16, 4, 16, 4, 16, 4, 16, 2, 2,
-};
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  4, 4, 4, 4, 4,
+  4, 16, 4, 16, 4, 16, 4, 16, 2, 2, 16,
 
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  6, 4, 12, 4, 4,
-  4, 4, 4, 4
-  
-  */
+  4, 16, 4, 16, 4, 16, 4, 16,
+  4, 16, 4, 16, 4, 16, 4, 16,
+  4, 16, 4, 16, 4, 16, 4, 16, 2, 2, 16,
+
+  4, 16, 4, 16, 4, 16, 4, 16,
+  4, 16, 4, 16, 4, 16, 4, 16,
+  6, 16, 4, 12, 16, 4, 16, 4, 16,
+  4, 16, 4, 16, 4, 16, 4, 16,
+};
 
 void setup() {
   for (int thisNote = 0; thisNote < numNotes; thisNote++) {
@@ -100,13 +77,6 @@ void setup() {
     delay(noteDuration);
     noTone(buzzerPin);
   }
-  /*
-  for (int i = 0; i < numNotes2; i++) {
-    int noteDuration2 = 1702 / noteDurations2[i];
-    tone(buzzerPin, melody2[i], noteDuration2);
-    delay(noteDuration2);
-  }
-  */
 }
 
 void loop() {
