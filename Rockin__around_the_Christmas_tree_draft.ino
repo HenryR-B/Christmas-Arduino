af8d7714f17e928f52be7fec7064d2aeb8709d46
Henry <henry.bryant70@gmail.com>
2017-12-21 14:30:39 -0800

    working version possibly not quite accurate to real tempo

--- Rockin__around_the_Christmas_tree_draft.ino
+++ Rockin__around_the_Christmas_tree_draft.ino
@@ -12,8 +12,9 @@ const int Bb4 = 466;
 const int R = 0;
 
 int buzzerPin = 14;
-int numNotes = 256;
-// notes in the melody:
+int numNotes = 244;
+int endingNumNotes1 = 12;
+int endingNumNotes2 = 19;
 int repeatedMelody[] = {
   Eb4, R, Eb4, R, Eb4, R, F4, R, F4, R,
   Eb4, R, Eb4, R, F4, F4, R, Eb4, R, Eb4, R,
@@ -47,13 +48,11 @@ int repeatedMelody[] = {
   F4, R, F4, R, F4, R, G4, R, G4, R,
   F4, R, F4, R, G4, G4, R, F4, R, F4, R,
 };
-int melody[] = {
-  repeatedMelody,
+int endingMelody1[] = {
   Eb4, R, E4, R, F4, R, G3, R, Ab3, Ab3, R, R,        //ending 1
+};
 
-  ////////////////////////////////////////////////////// the next part of code is exactly the same but with a different ending
-
-  repeatedMelody,
+int endingMelody2[] = {
   Eb4, R, R, E4, R, R, F4, R, R, G4, R, R, Bb4, Ab4, Ab4, Ab4, R, R, R,        //ending 2
 };
 
@@ -91,27 +90,44 @@ int repeatedNoteDurations[] = {
   4, 16, 6, 16, 12, 4, 16, 6, 16, 12, 16,
 };
 // note durations: 4 = quarter note, 16 = eighth note, etc.:
-int noteDurations[] = {
-  repeatedNoteDurations[],
+int endingNoteDurations1[] = {
   4, 16, 4, 16, 4, 16, 4, 16, 2, 4, 4, 16,      //Ending 1
-
-  repeatedNoteDurations[],
-  4, 4, 16, 4, 4, 16, 4, 4, 16, 4, 4, 16, 12, 6, 4, 1, 2, 1, 1,     //Ending 1
-  
+};
+int endingNoteDurations2[] = {
+  4, 4, 16, 4, 4, 16, 4, 4, 16, 4, 4, 16, 12, 6, 4, 1, 2, 1, 1,     //Ending 2
 };
 
 void setup() {
-  for (int thisNote = 0; thisNote < numNotes; thisNote++) {
-
-    int noteDuration = 1702 / noteDurations[thisNote];
-    tone(buzzerPin, melody[thisNote], noteDuration);
-    delay(noteDuration);
-    noTone(buzzerPin);
-  }
 }
 
 void loop() {
-  // no need to Repeat the melody.
+
+  for (int thisNote = 0; thisNote < numNotes; thisNote++) {
+
+    int noteDuration1 = 1702 / repeatedNoteDurations[thisNote];
+    tone(buzzerPin, repeatedMelody[thisNote], noteDuration1);
+    delay(noteDuration1);
+    noTone(buzzerPin);
+  }
+  for (int thisNote1 = 0; thisNote1 < endingNumNotes1; thisNote1++) {
+    int endingNoteDuration1 = 1702 / endingNoteDurations1[thisNote1];
+    tone(buzzerPin, endingMelody1[thisNote1], endingNoteDuration1);
+    delay(endingNoteDuration1);
+    noTone(buzzerPin);
+  }
+  for (int thisNote = 0; thisNote < numNotes; thisNote++) {
+
+    int noteDuration1 = 1702 / repeatedNoteDurations[thisNote];
+    tone(buzzerPin, repeatedMelody[thisNote], noteDuration1);
+    delay(noteDuration1);
+    noTone(buzzerPin);
+  }
+  for (int thisNote2 = 0; thisNote2 < endingNumNotes2; thisNote2++) {
+    int endingNoteDuration2 = 1702 / endingNoteDurations2[thisNote2];
+    tone(buzzerPin, endingMelody2[thisNote2], endingNoteDuration2);
+    delay(endingNoteDuration2);
+    noTone(buzzerPin);
+  }
 }
 
 
